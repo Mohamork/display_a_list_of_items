8237cc929ee2937ed0fb9d44e554fa06ae11b916
Mohamork <k.m19981102@gmail.com>
2026-01-13 20:20:09 +0100

    Update views.py
    add a view to get series from db

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from views import Serie
 
 # Create your views here.
+
+def get_serie(request) :
+    series = Serie.objects.all()
+    return render(request,'serie.html',{'series': series})
